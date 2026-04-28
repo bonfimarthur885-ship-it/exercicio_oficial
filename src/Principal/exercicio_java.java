31c89a9f867b66754662dea7bfe24b175a401513
IdeaPad <IdeaPad@10.146.192.135>
2026-04-28 14:42:17 -0300

    meus exercicios feitos em aulad

---
+++ src/Principal/exercicio_java.java
@@ -0,0 +1,18 @@
+ 
+package Principal;
+
+import javax.swing.JOptionPane;
+
+ 
+public class exercicio_java {
+
+     
+    public static void main(String[] args) {
+         String nome;
+         nome = JOptionPane.showInputDialog("Digite seu nome");
+         JOptionPane.showMessageDialog(null,"Seu nome é: " + nome);
+                
+        
+    }
+
+}
